465dd54f684e70989800e8d72f68047a2960f69a
yonn21 <87416721+yonn21@users.noreply.github.com>
2023-04-09 14:16:47 +0700

    Delete server/models directory

--- server/models/country.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const countrySchema = mongoose.Schema({
-    country: { type: String },
-    country_movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-})
-
-module.exports = mongoose.model('Country', countrySchema)
--- server/models/year.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const yearSchema = mongoose.Schema({
-    year: { type: Number },
-    year_movies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-})
-
-module.exports = mongoose.model('Year', yearSchema)
